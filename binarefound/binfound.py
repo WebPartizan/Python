5f4f0224dd4026c62b754b3c7ed82e981419f3cd
Oleg Atamankin <aov013@mail.ru>
2018-10-16 23:48:20 +0300

    New correct

--- binarefound/binfound.py
+++ binarefound/binfound.py
@@ -1,19 +1,21 @@
 def binary_search (list, item):
     low = 0
     hight = len(list) - 1
+    a = 0
 
     while low <= hight:
+        a += 1
         mid = int((low + hight)/2)
         guess = list[mid]
         if guess == item:
-            return mid
+            return mid, a
         if guess > item:
             hight = mid - 1
         else:
             low = mid + 1
     return None
 
-my_list = list(range(0,100))
+my_list = list(range(0,100,2))
 #print (my_list)
 print(binary_search(my_list, 3))
 print(binary_search(my_list, 98))
